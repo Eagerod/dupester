11803bc66ad29688af9aa8efaa7497380b41e070
Aleem Haji <hajial@gmail.com>
2022-01-13 20:55:37 -0500

    Save data to elasticsearch.

--- cmd/dupester/single_use.go
+++ cmd/dupester/single_use.go
@@ -1,16 +1,25 @@
 package dupester
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 )
 
 import (
+	es "github.com/elastic/go-elasticsearch/v7"
+	esapi "github.com/elastic/go-elasticsearch/v7/esapi"
 	"github.com/google/go-tika/tika"
 	"github.com/spf13/cobra"
 )
+
+type ESDoc struct {
+	Source string
+	Body string
+}
 
 func runCmd() *cobra.Command {
 	return &cobra.Command{
@@ -20,20 +29,50 @@ func runCmd() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Printf("Running against %s\n", args[0])
 
-			serverUrl := "http://dev.internal.aleemhaji.com:9998"
-			tikaClient := tika.NewClient(nil, serverUrl)
+			rContext := context.Background()
+			tikaServerUrl := "http://dev.internal.aleemhaji.com:9998"
+			tikaClient := tika.NewClient(nil, tikaServerUrl)
 
 			file, err := os.Open(args[0])
 			if err != nil {
 				return err
 			}
 
-			bs, err := tikaClient.ParseRecursive(context.Background(), file)
+			bs, err := tikaClient.ParseRecursive(rContext, file)
 			if err != nil {
 				return err
 			}
 
-			fmt.Println(strings.Join(bs, "\n"))
+			elasticsearchServerUrl := "http://dev.internal.aleemhaji.com:9200"
+
+			esCfg := es.Config{
+				Addresses: []string{
+				elasticsearchServerUrl,
+				},
+			  }
+
+			elasticsearchClient, err := es.NewClient(esCfg)
+			if err != nil {
+				return err
+			}
+
+			o := ESDoc{args[0], strings.Join(bs, "\n")}
+			b, err := json.Marshal(o)
+			if err != nil {
+				return err
+			}
+
+			req := esapi.IndexRequest{
+				Index:      "test",
+				Body:       bytes.NewReader(b),
+			  }
+
+			res, err := req.Do(rContext, elasticsearchClient)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(res)
 
 			return nil
 		},
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Eagerod/dupester
 go 1.16
 
 require (
+	github.com/elastic/go-elasticsearch/v7 v7.16.0 // indirect
 	github.com/google/go-tika v0.2.0 // indirect
 	github.com/spf13/cobra v1.3.0 // indirect
 )
