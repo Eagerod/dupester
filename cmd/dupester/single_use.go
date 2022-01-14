1e54b0f24cecb106eab085c24e0d66f09c435b62
Aleem Haji <hajial@gmail.com>
2022-01-13 20:34:06 -0500

    Run tika against an input file.

--- cmd/dupester/single_use.go
+++ cmd/dupester/single_use.go
@@ -1,25 +1,40 @@
 package dupester
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"strings"
 )
 
 import (
+	"github.com/google/go-tika/tika"
 	"github.com/spf13/cobra"
 )
-
-// import (
-// 	"github.com/google/go-tika"
-// )
-
 
 func runCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "run",
 		Short: "run against a single local file",
-		Args: cobra.ArbitraryArgs,
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("Ran")
+			fmt.Printf("Running against %s\n", args[0])
+
+			serverUrl := "http://dev.internal.aleemhaji.com:9998"
+			tikaClient := tika.NewClient(nil, serverUrl)
+
+			file, err := os.Open(args[0])
+			if err != nil {
+				return err
+			}
+
+			bs, err := tikaClient.ParseRecursive(context.Background(), file)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(strings.Join(bs, "\n"))
+
 			return nil
 		},
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Eagerod/dupester
 
 go 1.16
 
-require github.com/spf13/cobra v1.3.0 // indirect
+require (
+	github.com/google/go-tika v0.2.0 // indirect
+	github.com/spf13/cobra v1.3.0 // indirect
+)
