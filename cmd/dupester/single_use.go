c5ff38a508cfd4e27e46b233404e51ff317557a0
Aleem Haji <hajial@gmail.com>
2022-01-14 19:26:53 -0500

    Moves single-use logic to pkg.

--- cmd/dupester/single_use.go
+++ cmd/dupester/single_use.go
@@ -1,23 +1,15 @@
 package dupester
 
 import (
-	"bytes"
-	"context"
-	"crypto/sha256"
-	"encoding/hex"
-	"encoding/json"
 	"fmt"
-    "io/ioutil"
-	"os"
-    "regexp"
-	"strings"
 )
 
 import (
-	es "github.com/elastic/go-elasticsearch/v7"
-	esapi "github.com/elastic/go-elasticsearch/v7/esapi"
-	"github.com/google/go-tika/tika"
 	"github.com/spf13/cobra"
+)
+
+import (
+	"github.com/Eagerod/dupester/pkg/dupester"
 )
 
 type ESDoc struct {
@@ -34,89 +26,27 @@ func runCmd() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Printf("Running against %s\n", args[0])
 
-			rContext := context.Background()
-			tikaServerUrl := "http://dev.internal.aleemhaji.com:9998"
-			tikaClient := tika.NewClient(nil, tikaServerUrl)
-
-			file, err := os.Open(args[0])
+			ds, err := dupester.NewDupester("http://dev.internal.aleemhaji.com:9998", "http://dev.internal.aleemhaji.com:9200")
 			if err != nil {
 				return err
 			}
 
-			fileContents, err := ioutil.ReadFile(args[0])
-			if err != nil {
-				return err
-			}
-			hash := sha256.Sum256(fileContents)
-
-			bodyArray, err := tikaClient.ParseRecursive(rContext, file)
+			doc, err := ds.ParseFile(args[0])
 			if err != nil {
 				return err
 			}
 
-			originalBody := strings.Join(bodyArray, "\n")
-
-			re := regexp.MustCompile("[\\.$-/:-?{-~!\"^_`\\[\\]]")
-			bodyString := re.ReplaceAllString(originalBody, " ")
-
-			elasticsearchServerUrl := "http://dev.internal.aleemhaji.com:9200"
-
-			esCfg := es.Config{
-				Addresses: []string{
-					elasticsearchServerUrl,
-				},
-			}
-
-			elasticsearchClient, err := es.NewClient(esCfg)
+			docs, err := ds.FindLike(doc)
 			if err != nil {
 				return err
 			}
 
-			o := ESDoc{args[0], originalBody, bodyString}
-			b, err := json.Marshal(o)
+			fmt.Printf("Found %d docs like %s\n", len(docs), args[0])
+
+			err = ds.Save(doc)
 			if err != nil {
 				return err
 			}
-
-			var buf bytes.Buffer
-			query := map[string]interface{}{
-			  "query": map[string]interface{}{
-				"more_like_this": map[string]interface{}{
-				  "fields": []string{"body"},
-				  "like": o.Body,
-				  "min_term_freq": 1,
-				  "min_doc_freq": 1,
-				  "max_query_terms": 1000,
-				  "analyzer": "whitespace",
-				},
-			  },
-			}
-
-			if err := json.NewEncoder(&buf).Encode(query); err != nil {
-			  return fmt.Errorf("Error encoding query: %s", err)
-			}
-
-			// Perform the search request.
-			res, err := elasticsearchClient.Search(
-				elasticsearchClient.Search.WithContext(context.Background()),
-				elasticsearchClient.Search.WithIndex("test"),
-				elasticsearchClient.Search.WithBody(&buf),
-			)
-
-			fmt.Println(res)
-
-			indexRequest := esapi.IndexRequest{
-				DocumentID: hex.EncodeToString(hash[:]),
-				Index:      "test",
-				Body:       bytes.NewReader(b),
-			  }
-
-			indexResponse, err := indexRequest.Do(rContext, elasticsearchClient)
-			if err != nil {
-				return err
-			}
-
-			fmt.Println(indexResponse)
 
 			return nil
 		},
---
+++ pkg/dupester/dupester.go
@@ -0,0 +1,162 @@
+package dupester
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+    "io/ioutil"
+	"os"
+    "regexp"
+	"strings"
+)
+
+import (
+	es "github.com/elastic/go-elasticsearch/v7"
+	esapi "github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/google/go-tika/tika"
+)
+
+type ExtractedDocument struct {
+	Source string `json:"source"`
+	Hash string `json:"hash"`
+	OriginalBody string `json:"originalBody"`
+	Body string `json:"body"`
+}
+
+type Dupester struct {
+	tikaClient *tika.Client
+	elasticsearchClient *es.Client
+}
+
+func NewDupester(tikaServerUrl, elasticsearchServerUrl string) (*Dupester, error) {
+	d := &Dupester{}
+
+	d.tikaClient = tika.NewClient(nil, tikaServerUrl)
+
+	esCfg := es.Config{
+		Addresses: []string{
+			elasticsearchServerUrl,
+		},
+	}
+
+	elasticsearchClient, err := es.NewClient(esCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	d.elasticsearchClient = elasticsearchClient
+
+	return d, nil
+}
+
+func (dupester *Dupester) ParseFile(path string) (*ExtractedDocument, error) {
+	fileContents, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	hash := sha256.Sum256(fileContents)
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyLines, err := dupester.tikaClient.ParseRecursive(context.Background(), file)
+	if err != nil {
+		return nil, err
+	}
+
+	originalBody := strings.Join(bodyLines, "\n")
+
+	re := regexp.MustCompile("[\\.$-/:-?{-~!\"^_`\\[\\]]")
+	bodyString := re.ReplaceAllString(originalBody, " ")
+
+	rv := &ExtractedDocument{}
+	rv.Source = path
+	rv.Hash = hex.EncodeToString(hash[:])
+	rv.OriginalBody = originalBody
+	rv.Body = bodyString
+	
+	return rv, nil
+}
+
+func (dupester *Dupester) FindLike(doc *ExtractedDocument) ([]ExtractedDocument, error) {
+	var buf bytes.Buffer
+	query := map[string]interface{}{
+	  "query": map[string]interface{}{
+		"more_like_this": map[string]interface{}{
+		  "fields": []string{"body"},
+		  "like": doc.Body,
+		  "min_term_freq": 1,
+		  "min_doc_freq": 1,
+		  "max_query_terms": 1000,
+		  "analyzer": "whitespace",
+		},
+	  },
+	}
+
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+	  return nil, fmt.Errorf("Error encoding query: %s", err)
+	}
+
+	res, err := dupester.elasticsearchClient.Search(
+		dupester.elasticsearchClient.Search.WithContext(context.Background()),
+		dupester.elasticsearchClient.Search.WithIndex("test"),
+		dupester.elasticsearchClient.Search.WithBody(&buf),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Failed to query more like this, %s", res.Body)
+	}
+
+    buf.ReadFrom(res.Body)
+
+	var esWrapper = struct {
+		Hits struct {
+			Hits []struct {
+				Source ExtractedDocument `json:"_source"`
+			 } `json:"hits"`
+		} `json:"hits"`
+	} {}
+
+    err = json.Unmarshal(buf.Bytes(), &esWrapper)
+	if err != nil {
+		return nil, nil
+	}
+
+	rv := []ExtractedDocument{}
+	for _, o := range esWrapper.Hits.Hits {
+		rv = append(rv, o.Source)
+	}
+
+	return rv, nil
+}
+
+func (dupester *Dupester) Save(doc *ExtractedDocument) error {
+	b, err := json.Marshal(doc)
+	if err != nil {
+		return err
+	}
+
+	indexRequest := esapi.IndexRequest{
+		DocumentID: doc.Hash,
+		Index:      "test",
+		Body:       bytes.NewReader(b),
+	  }
+
+	indexResponse, err := indexRequest.Do(context.Background(), dupester.elasticsearchClient)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(indexResponse)
+
+	return nil
+}
