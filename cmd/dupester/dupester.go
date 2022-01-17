c2a0f6d7c394f8e55dcbc12399886471e98cdb05
Aleem Haji <hajial@gmail.com>
2022-01-16 20:00:59 -0500

    Enable confiuration by file.

--- cmd/dupester/dupester.go
+++ cmd/dupester/dupester.go
@@ -1,30 +1,36 @@
 package dupester
 
 import (
+	"fmt"
+	"os"
+)
+
+import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 import (
 	"github.com/Eagerod/dupester/pkg/dupester"
 )
 
+var cfgFile string
 var dupesterClient *dupester.Dupester
 
 func init() {
-	ds, err := dupester.NewDupester("http://dev.internal.aleemhaji.com:9998", "http://dev.internal.aleemhaji.com:9200")
-	if err != nil {
-		panic(err)
-	}
-
-	dupesterClient = ds
+	cobra.OnInitialize(initConfig, initDupester)
 }
 
 func rootCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "dupester",
 		Short: "command line tool for finding duplicates, or something",
 		Args:  cobra.ArbitraryArgs,
 	}
+
+	cmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dupester.yaml)")
+
+	return cmd
 }
 
 func Run() error {
@@ -34,4 +40,47 @@ func Run() error {
 	cmd.AddCommand(checkCmd())
 
 	return cmd.Execute()
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".dupester" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".dupester")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func initDupester() {
+	var tikaString string
+	viper.UnmarshalKey("tika", &tikaString)
+
+	var elasticsearchString string
+	viper.UnmarshalKey("elasticsearch", &elasticsearchString)
+
+	ds, err := dupester.NewDupester(tikaString, elasticsearchString)
+	if err != nil {
+		panic(err)
+	}
+
+	dupesterClient = ds
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/elastic/go-elasticsearch/v7 v7.16.0 // indirect
 	github.com/google/go-tika v0.2.0 // indirect
 	github.com/spf13/cobra v1.3.0 // indirect
+	github.com/spf13/viper v1.10.1 // indirect
 )
